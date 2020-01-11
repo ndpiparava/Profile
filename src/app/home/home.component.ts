2ab016afb35e69c1424f75c794ef2aa957f3fc56
ndpiparava <ndpiparava@gmail.com>
2020-01-11 21:32:36 +0530

    added site title,description,keywords and made skills component dynamic to have smaller html clode

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+// Model class to hold skill data.
+class Skill {
+  constructor(public skill: String, public expertise: number) {}
+}
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public mySkills: Skill[] = [
+    new Skill("Objective-C", 80),
+    new Skill("Swift", 75),
+    new Skill("Angular", 75),
+    new Skill("HTML", 80),
+    new Skill("CSS", 80),
+    new Skill("Bootstrap", 85)
+  ];
 
-  ngOnInit() {
-  }
+  public test = "80%";
 
+  ngOnInit() {}
 }
