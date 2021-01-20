8cb85cf862cf1752464d996e8c9ea216fc767662
npiparav <ndpiparava@gmail.com>
2021-01-20 13:26:16 +0530

    Corrected skill level

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,9 +15,9 @@ export class HomeComponent implements OnInit {
 
   public mySkills: Skill[] = [
     new Skill("Swift", 90),
-    new Skill("Objective-C", 79),
-    new Skill("React Native", 76),
-    new Skill("JavaScript", 73),
+    new Skill("Objective-C", 83),
+    new Skill("React Native", 83),
+    new Skill("JavaScript", 76),
     new Skill("HTML", 76),
     new Skill("CSS", 83),
     new Skill("Bootstrap", 73)
